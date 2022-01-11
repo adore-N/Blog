37ca6f88ae1fc04a5896a2e786f34f0db64c4aae
adore-N <819790662@qq.com>
2022-01-11 17:36:49 +0800

    test

--- node_blog/app.js
+++ node_blog/app.js
@@ -49,6 +49,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+console.log(1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
